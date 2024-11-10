e97f16628cb5d9976c3ac84a31ae95ad7978b791
YoussoufYk <101326105+YoussoufYk@users.noreply.github.com>
2024-11-10 06:11:45 +0100

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -25,7 +25,7 @@ const config = {
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "ɢɪғᴛᴇᴅ ᴛᴇᴄʜ",
   AUTO_BLOCK: process.env.AUTO_BLOCK || "212,99,90",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254762016957",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "237696262128",
   SUDO_NUMBERS: process.env.SUDO_NUMBERS || "254762016957,254728746852,254742063632",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyAkHGe6qWMujR2BjM468vQbY2RIbp0VXyc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
